15449ac436ab2256cb89c785df75dd068103a43d
nguyễn kiều chinh <67265709+601272kieuchinh@users.noreply.github.com>
2020-06-22 17:27:16 +0700

    CODE LTHDT
    khung code lap trinh huong doi tuong

---
+++ khung.cpp
@@ -0,0 +1,32 @@
+//Ho ten: Nguyen Kieu Chinh
+//MaSV:601272
+//Lop:K60QLTT
+//De:
+
+#include<iostream>
+#include<stdio.h>
+
+using namespace std;
+
+//===khai bao lop===
+
+class A
+{
+    private:
+
+    public:
+
+};
+
+//===chuong trinh chinh===
+
+int main()
+{
+
+    cout<<endl;
+    return 0;
+}
+
+//===dinh nghia ham===
+
+
